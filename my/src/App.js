832f7a6c6fa30c3eb6f134aea1198e04e70ed75b
Victor Valente <Valente-victor@hotmail.com>
2023-08-16 17:19:11 -0300

    Hook useState

--- my/src/App.js
+++ my/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './Header';
+import React, { useState } from 'react';
 
 // COMPONENTE APP
 function App() {
+
+  // Exemplo de HOOK useState
+  const [count, setCount] = useState(0);
+
   return(
-    <div className='App'>
-      <Header/>
+    <div>
+      <p>Click contador: {count} vezes</p>
+      <button onClick={() => setCount(count + 1 )}>
+        click em mim
+      </button>
     </div>
   );
   
