e615d041dc28aae46124772293c7a7756e39440f
Victor Valente <Valente-victor@hotmail.com>
2023-08-16 17:26:52 -0300

    Hook useEffect

--- my/src/App.js
+++ my/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Header from './Header';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // COMPONENTE APP
 function App() {
 
   // Exemplo de HOOK useState
   const [count, setCount] = useState(0);
+
+  function callAlert(){
+    alert('Seja bem vindo ao react bascis');
+  };
+
+  useEffect(()=>{
+    // Atualiza o titulo do documento usando API do browser
+    document.title = 'Voce clickou: ' + count + " vezes";
+  },[count]); //  Atualiza quando mudar o count
+
+  useEffect(()=>{
+    callAlert();
+  },[]); //  Atualiza so uma vez
 
   return(
     <div>
